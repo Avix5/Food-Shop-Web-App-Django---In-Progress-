8148de9d9ef59a63d552ca274d470c1de2ac2f2f
Abhishek Yadav <167422303+Avix5@users.noreply.github.com>
2026-03-21 21:21:06 +0530

    Implement CRUD operations for items
    Added item creation, update, and delete functionality.

--- views.py
+++ views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import item
+from .forms import ItemForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
     # getting data from database
     item_list=item.objects.all()
@@ -26,3 +29,37 @@ def detail(request,id):
 
 def item_view(request):
     return HttpResponse("this is an item")
+
+def create_item(request):
+    if request.method=="POST":
+        form=ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        # print("post is trigerred")
+        # print(request.POST)
+    form =ItemForm()
+    context={
+        "form":form
+    }
+    return render(request,"myapp/item-form.html",context)
+
+def update_item(request,id):    
+    # print("you can edit product of id",id)
+    Item=item.objects.get(id=id)
+    form=ItemForm(request.POST or None,instance=Item)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    context={
+        "form":form
+    }
+    return render(request,'myapp/item-form.html',context)
+
+def delete_item(request,id):
+    Item=item.objects.get(id=id)
+    if request.method=="POST":
+        Item.delete()
+        return redirect("index")
+    
+    return render(request,'myapp/delete.html')
