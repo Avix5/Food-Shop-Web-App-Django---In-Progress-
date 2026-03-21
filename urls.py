91a3263175d36ad6f014d76da61568119c804584
Abhishek Yadav <167422303+Avix5@users.noreply.github.com>
2026-03-21 21:20:19 +0530

    Update URL patterns for item management

--- urls.py
+++ urls.py
@@ -1,17 +1,13 @@
 from django.urls import path
 from . import views
+
+# app_name = 'myapp'
+
 urlpatterns = [
-    path('',views.index),
-    path('item',views.item),
+    path('',views.index,name='index'),
     path('<int:id>/',views.detail,name='detail'),  
     # this is used to handel dynamic id 
+    path('add/',views.create_item,name='create_item'),
+    path('update/<int:id>/',views.update_item,name='update_item'),
+    path('delete/<int:id>/',views.delete_item,name="delete_item"),
 ]
-
-
-
-
-# admin data
-# name=abhishek
-# pas=superuser
-# mail=abhishek@gmail.com
-# 
