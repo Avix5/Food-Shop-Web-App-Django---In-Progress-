1f69ed5d1a7bee1da368bcfb2de7ab7028f4fbf3
Abhishek Yadav <167422303+Avix5@users.noreply.github.com>
2026-03-22 22:56:05 +0530

    Refactor views to use class-based views for index and detail

--- views.py
+++ views.py
@@ -3,28 +3,46 @@ from django.http import HttpResponse
 from .models import item
 from .forms import ItemForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 
-@login_required
-def index(request):
-    # getting data from database
-    item_list=item.objects.all()
-    # return HttpResponse(item_list)
-    # creating context
-    context={
-        'item_list':item_list
-    }
-    # passing context to html
-    return render(request,"myapp/index.html",context)
+# @login_required
+# function based view
+
+# def index(request):
+#     # getting data from database
+#     item_list=item.objects.all()
+#     # return HttpResponse(item_list)
+#     # creating context
+#     context={
+#         'item_list':item_list
+#     }
+#     # passing context to html
+#     return render(request,"myapp/index.html",context)
+
+
+# class based view
+class IndexClassView(ListView):
+    model=item
+    template_name="myapp/index.html"
+    context_object_name='item_list'
 
 # getting data on the basis of id
-def detail(request,id):
-    Item=item.objects.get(id=id)
-    # return HttpResponse(f"this is detail view of id {Item}")
-    context={
-        'item':Item
-    }
-    return render(request,"myapp/details.html",context)
+# def detail(request,id):
+#     Item=item.objects.get(id=id)
+#     # return HttpResponse(f"this is detail view of id {Item}")
+#     context={
+#         'item':Item
+#     }
+#     return render(request,"myapp/details.html",context)
+
+# class based vieew
+class FoodDetail(DetailView):
+    model=item
+    template_name="myapp/details.html"
+    context_object_name='item'
+
 
 
 def item_view(request):
